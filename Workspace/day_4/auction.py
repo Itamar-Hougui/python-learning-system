25a3f079442f412456f51ddd54b2f1a193e332cb
Itamar Hougui <itamar.hougui@adelaide.edu.au>
2025-07-03 22:19:06 +0930

    day 5 - finishing day 4 auction program

--- Workspace/day_4/auction.py
+++ Workspace/day_4/auction.py
@@ -1,3 +1,4 @@
+import os
 
 print("Welcome to the secret auction program.")
 first_bidder=input("What is your name? ")
@@ -6,13 +7,14 @@ bids={
     first_bidder:first_bid
 }
 more_bidder=input("Are there any other bidders? type 'yes' or 'no'").lower()
-
+highest_bid=bids[first_bidder]
+highest_bider=first_bidder
 if more_bidder=="yes":
     auction_on=True
 else:
     auction_on=False
 while(auction_on):
-    print("\n"*100)
+    os.system('clear')
     new_bidder=input("What is your name? ")
     new_bid=int(input("What is your bid? $"))
     bids[new_bidder]=new_bid
@@ -20,4 +22,8 @@ while(auction_on):
     if more_bidder=="no":
         auction_on=False
 
-highest_bid
+for name in bids:
+    if (bids[name]>highest_bid):
+        highest_bid=bids[name]
+        highest_bider=name
+print(f"The highest bidder is {highest_bider}\nWho bids ${highest_bid}")
