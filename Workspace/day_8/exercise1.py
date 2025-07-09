bb86bfbff77f2a26d83158f6a9feb9181f9728e7
Itamar Hougui <itamar.hougui@adelaide.edu.au>
2025-07-09 23:17:03 +0930

    9/7/25 still need to finish quiz mini project

---
+++ Workspace/day_8/exercise1.py
@@ -0,0 +1,9 @@
+import turtle
+
+tom = turtle.Turtle()
+tom.shape("turtle")
+tom.color("coral","DarkOrchid")
+tom.forward(100)
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
